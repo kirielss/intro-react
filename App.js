72ca3569fa6977ed5b5c6136c26649fdd563de2e
skiriel <gabrieldllopes@gmail.com>
2022-11-21 16:34:36 -0300

    link bug

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { render } from "react-dom";
 import { StrictMode } from "react";
 import SearchParams from "./SearchParams.jsx";
+import Details from "./details.js";
 
 const App = () => {
   return (
     <StrictMode>
-
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+      <BrowserRouter>
+        <h1>Adopt Me!</h1>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
     </StrictMode>
   );
 };
