efc9921270c4d4b44359eda71d9cf66796269748
skiriel <gabrieldllopes@gmail.com>
2022-11-21 12:10:16 -0300

    core react concepts

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
+import { StrictMode } from "react";
 import SearchParams from "./SearchParams.jsx";
 
 const App = () => {
   return (
+    <StrictMode>
+
     <div>
       <h1>Adopt Me!</h1>
       <SearchParams />
     </div>
+    </StrictMode>
   );
 };
 
