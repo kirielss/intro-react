c0808a762b32306b0f8b1f790c5e8747bc3ab7e7
skiriel <gabrieldllopes@gmail.com>
2022-11-21 17:41:37 -0300

    react-router ok

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { render } from "react-dom";
 import { StrictMode } from "react";
 import SearchParams from "./SearchParams.jsx";
@@ -9,7 +9,10 @@ const App = () => {
   return (
     <StrictMode>
       <BrowserRouter>
-        <h1>Adopt Me!</h1>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+
         <Routes>
           <Route path="/details/:id" element={<Details />} />
           <Route path="/" element={<SearchParams />} />
