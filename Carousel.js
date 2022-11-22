11392ba19e328046bf04c77ec56602314857c9a4
skiriel <gabrieldllopes@gmail.com>
2022-11-22 10:22:44 -0300

    carousel ok

--- Carousel.js
+++ Carousel.js
@@ -11,6 +11,12 @@ class Carousel extends Component {
         ]
     }
 
+    handleIndexClick = (event) => {
+        this.setState({
+            active: +event.target.dataset.index
+        })
+    }
+
     render() {
         const { active } = this.state;
         const { images } = this.props;
@@ -20,8 +26,14 @@ class Carousel extends Component {
                 <img src={images[active]} alt='animal' />
                 <div className="carousel-smaller">
                     {images.map((photo, index) => (
+                        // eslint-disable-next-line
                         <img
-                            key={photo} src={photo} className={index === active ? "active" : ""} alt="animal thumbnail"
+                            onClick={this.handleIndexClick}
+                            key={photo}
+                            src={photo}
+                            data-index={index}
+                            className={index === active ? "active" : ""}
+                            alt="animal thumbnail"
                         />
                     ))}
                 </div>
